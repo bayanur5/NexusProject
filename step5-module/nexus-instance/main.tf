3d79ee6aa9398246a3171a6e72bfb4cf8727bf2d
bayanur5 <b.dzhoroeva@gmail.com>
2026-03-08 05:20:55 +0000

    changed

--- step5-module/nexus-instance/main.tf
+++ step5-module/nexus-instance/main.tf
@@ -1,13 +1,19 @@
+# Automatically get the latest Ubuntu 22.04 LTS AMI in us-east-1
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]  # Official Canonical owner
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
 resource "aws_instance" "nexus" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name      = var.key_name
 
   tags = {
-    Name = "NexusInstance"
+    Name = "nexus-instance"
   }
-}
-
-output "instance_id" {
-  value = aws_instance.nexus.id
 }
--- step5-module/nexus-instance/variables.tf
+++ step5-module/nexus-instance/variables.tf
@@ -1,3 +1,13 @@
-variable "ami_id" {}
-variable "key_name" {}
-variable "instance_type" {}
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.medium"
+}
