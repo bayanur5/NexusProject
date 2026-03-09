d94795b004daf107217a3afb2947512a76a2aac4
bayanur5 <b.dzhoroeva@gmail.com>
2026-03-09 14:25:30 +0000

    modified step5

--- step5-module/main.tf
+++ step5-module/main.tf
@@ -1,3 +1,61 @@
+provider "aws" {
+  region = var.region
+}
+
+# Fetch the latest Nexus AMI created by Packer
+data "aws_ami" "nexus_latest" {
+  most_recent = true
+  owners      = ["self"]  # your AWS account
+  filter {
+    name   = "name"
+    values = ["nexus-ami-*"]
+  }
+}
+
+# Security Group for Nexus
+resource "aws_security_group" "nexus_sg" {
+  name        = "nexus-sg"
+  description = "Allow SSH and Nexus HTTP"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Nexus EC2 Instance
+resource "aws_instance" "nexus" {
+  ami                         = data.aws_ami.nexus_latest.id
+  instance_type                = var.instance_type
+  subnet_id                    = var.public_subnet_ids[0]
+  key_name                     = var.key_name
+  vpc_security_group_ids       = [aws_security_group.nexus_sg.id]
+  associate_public_ip_address  = true
+
+  tags = {
+    Name    = "nexus-server"
+    Project = "NexusTest"
+  }
+}
+
+# Optional: wait until Nexus responds on port 8081
 resource "null_resource" "wait_for_nexus" {
   depends_on = [aws_instance.nexus]
 
@@ -6,7 +64,7 @@ resource "null_resource" "wait_for_nexus" {
       type        = "ssh"
       host        = aws_instance.nexus.public_ip
       user        = "ubuntu"
-      private_key = file("/Users/aktan/.ssh/id_rsa")
+      private_key = file(var.private_key_path)
     }
 
     inline = [
--- step5-module/variables.tf
+++ step5-module/variables.tf
@@ -1,43 +1,36 @@
-# AWS Region
 variable "region" {
   description = "AWS region to deploy Nexus"
   type        = string
   default     = "us-east-1"
 }
 
-# Nexus AMI ID (from Packer build)
-variable "ami_id" {
-  description = "AMI ID for Nexus EC2 instance"
-  type        = string
-}
-
-# EC2 instance type
-variable "instance_type" {
-  description = "EC2 instance type for Nexus"
-  type        = string
-  default     = "t2.small"
-}
-
-# VPC where Nexus will be launched
 variable "vpc_id" {
-  description = "VPC ID to launch Nexus in"
+  description = "VPC ID for the Nexus instance"
   type        = string
 }
 
-# Public subnets for EC2 instance
 variable "public_subnet_ids" {
   description = "List of public subnet IDs"
   type        = list(string)
 }
 
-# SSH key name for connecting to EC2
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.small"
+}
+
 variable "key_name" {
-  description = "Name of the SSH key for EC2 instance"
+  description = "SSH key name"
   type        = string
 }
 
-# Your public IP for SSH access
 variable "my_ip" {
-  description = "Your laptop public IP with /32 suffix for SSH access"
+  description = "Your public IP for SSH access"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "Path to your private SSH key"
   type        = string
 }
